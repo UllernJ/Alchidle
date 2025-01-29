f7004947a483d525380ee763fb68f15db3412bd3
UllernJ <Ulrik.johannesen@gmail.com>
2025-01-29 23:04:39 +0100

    formatting

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,5 +1,23 @@
-import type Decimal from "break_eternity.js";
+import Decimal from "break_eternity.js";
 
 export const formatNumber = (num: Decimal): string => {
-  return num.round().toString() || "0";
+  if (num.gte(new Decimal(1e15))) {
+    return num.toExponential(2).replace("+", "");
+  }
+
+  const units = ["", "K", "M", "B", "T"];
+  let unitIndex = 0;
+  let value = num;
+
+  while (value.gte(new Decimal(1000)) && unitIndex < units.length - 1) {
+    value = value.div(new Decimal(1000));
+    unitIndex++;
+  }
+
+  let formattedValue = value.toFixed(2).replace(/\.00$/, "");
+  if (value.lt(new Decimal(1000))) {
+    formattedValue = value.toFixed(0);
+  }
+
+  return `${formattedValue}${units[unitIndex]}`;
 };
